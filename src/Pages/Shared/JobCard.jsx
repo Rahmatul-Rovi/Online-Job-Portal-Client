adf43bcf5dca8b55395ddfb1e24f7949f89fb4c3
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-01-13 20:38:12 +0600

    Job Card section updated

--- src/Pages/Shared/JobCard.jsx
+++ src/Pages/Shared/JobCard.jsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React from "react";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
-const JobCard = ({job}) => {
-    const {title, location, jobType, category, applicationDeadline, salaryRange, description, company} = job;
-    return (
-        <div className="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">
-      { title}
-      <div className="badge badge-secondary">NEW</div>
-    </h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
+const JobCard = ({ job }) => {
+  const {
+    title,
+    location,
+    jobType,
+    category,
+    requirements,
+    applicationDeadline,
+    salaryRange,
+    description,
+    company,
+    company_logo,
+  } = job;
+  return (
+    <div className="card bg-base-100 w-96 shadow-sm">
+      <div className="flex gap-2">
+        <figure>
+          <img src={company_logo} className="w-16" alt="Shoes" />
+        </figure>
+        <div>
+            <h3 className="text-4xl">{company}</h3>
+            <p className="flex gap-1 items-center"> <FaMapMarkerAlt /> {location}</p>
+        </div>
+      </div>
+      <div className="card-body">
+        <h2 className="card-title">
+          {title}
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>
+         {description}
+        </p>
+        <div className="card-actions">
+         {
+            requirements.map((skill, index) => <div
+            key={index}
+             className="badge badge-outline">{skill}</div>)
+         }
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default JobCard;
