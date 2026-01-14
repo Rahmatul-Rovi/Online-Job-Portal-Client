5bb7d9413eb8511df9f421adf7627416c7d3ee93
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-01-14 07:16:22 +0600

    Apply job section updated

--- src/Pages/JobApply/JobApply.jsx
+++ src/Pages/JobApply/JobApply.jsx
@@ -1,24 +1,37 @@
 import React from "react";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 import useAuth from "../../hooks/useAuth";
 
 const JobApply = () => {
   const { id: jobId } = useParams();
   const { user } = useAuth();
+
+  const handleApplyFormSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    const linkdln = form.linkdln.value;
+    const github = form.github.value;
+    const resume = form.resume.value;
+    console.log(linkdln, github, resume);
+
+  }
   return (
     <div>
-      <h3 className="text-4xl">Apply for job</h3>
-      <form>
+     <div className="flex gap-5">
+         <h3 className="text-4xl">Apply for job</h3>
+        <Link to={`/jobs/${jobId}`}>  <h4 className="text-4xl">Job Details </h4> </Link>
+     </div>
+      <form onSubmit={handleApplyFormSubmit}>
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
 
           <label className="label">Linkdln Profile</label>
-          <input type="url" className="input" placeholder="Linkdln profile link" />
+          <input type="url" name="linkdln" className="input" placeholder="Linkdln profile link" />
 
           <label className="label">Github Profile</label>
-          <input type="url" className="input" placeholder="Github profile link" />
+          <input type="url" name="github" className="input" placeholder="Github profile link" />
 
           <label className="label">Resume</label>
-          <input type="url" className="input" placeholder="Resume link" />
+          <input type="url" name="resume" className="input" placeholder="Resume link" />
 
           <button type="submit" className="btn">Apply</button>
         </fieldset>
