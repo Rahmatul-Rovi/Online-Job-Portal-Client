787102e710acf9b641d37a7a78a3531462301ecb
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-01-14 20:49:34 +0600

    Client Section Updated

--- src/Pages/MyApplications/ApplicationList.jsx
+++ src/Pages/MyApplications/ApplicationList.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { use } from 'react';
 
-const ApplicationList = () => {
+const ApplicationList = ({ myApplicationsPromise }) => {
+    const applications = use(myApplicationsPromise);
+
     return (
         <div>
-            
+            <h3 className='text-3xl'>
+                Jobs Applied so far: {applications?.length || 0}
+            </h3>
+            <ul>
+                {applications?.map(app => (
+                    <li key={app._id}>{app.job_title || "Job Application"}</li>
+                ))}
+            </ul>
         </div>
     );
 };
--- src/Pages/MyApplications/MyApplications.jsx
+++ src/Pages/MyApplications/MyApplications.jsx
@@ -1,13 +1,19 @@
 import React, { Suspense } from 'react';
 import ApplicationStar from './ApplicationStar';
 import ApplicationList from './ApplicationList';
+import useAuth from '../../hooks/useAuth';
+import { myApplicationsPromise } from '../../api/ApplicationsApi';
 
 const MyApplications = () => {
+    const { user } = useAuth();
+
+    if (!user?.email) return <div>Loading User...</div>;
+
     return (
         <div>
-            <ApplicationStar></ApplicationStar>
-            <Suspense fallback={'Loading your Applications'}>
-            <ApplicationList></ApplicationList>
+            <ApplicationStar />
+            <Suspense fallback={<div>Loading your Applications...</div>}>
+                <ApplicationList myApplicationsPromise={myApplicationsPromise(user.email)} />
             </Suspense>
         </div>
     );
