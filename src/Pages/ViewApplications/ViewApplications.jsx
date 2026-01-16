f87628f6545bcb65f5288fc7aba3802d729071d4
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-01-16 16:34:30 +0600

    Client Section Updated

--- src/Pages/ViewApplications/ViewApplications.jsx
+++ src/Pages/ViewApplications/ViewApplications.jsx
@@ -1,15 +1,74 @@
-import React from 'react';
-import { useLoaderData, useParams } from 'react-router';
+import axios from "axios";
+import React from "react";
+import { useLoaderData, useParams } from "react-router";
+import Swal from "sweetalert2";
 
 const ViewApplications = () => {
-    const {job_id} = useParams();
-    const applications = useLoaderData();
-    
-    return (
-        <div>
-            <h4 className="text-4xl">{applications.length} Application for: {job_id}</h4>
-        </div>
-    );
+  const { job_id } = useParams();
+  const applications = useLoaderData();
+
+  const handleStatusChange = (e, app_id) => {
+    // Note: status pathanor somoy target value nite hobe
+    axios
+      .patch(`http://localhost:3000/applications/${app_id}`, {
+        status: e.target.value,
+      })
+      .then((res) => {
+        if (res.data.modifiedCount) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Application Status Updated.",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div>
+      <h4 className="text-4xl">
+        {applications.length} Applications for: {job_id}
+      </h4>
+
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {applications.map((application, index) => (
+              <tr key={application._id}>
+                <th>{index + 1}</th>
+                <td>{application.applicant}</td>
+                <td>Quality Control Specialist</td>
+                <td>
+                  <select
+                    onChange={(e) => handleStatusChange(e, application._id)}
+                    defaultValue={application.status || "Update Status"}
+                    className="select select-bordered"
+                  >
+                    <option disabled={true}>Update Status</option>
+                    <option>Pending</option>
+                    <option>Interview</option>
+                    <option>Hired</option>
+                    <option>Rejected</option>
+                  </select>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ViewApplications;
